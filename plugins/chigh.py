1fcd532bd4f0bab7666fbb967205f7e51b745259
unknown <kingof.trex@yahoo.com>
2019-04-20 14:56:09 -0400

    Added functionality to chigh and rankhistory

--- plugins/chigh.py
+++ plugins/chigh.py
@@ -1,56 +1,83 @@
 import requests
 from lxml import html
-
+import datetime
 from cloudbot import hook
 
 
 @hook.command('chigh')
-def chigh(text):
-    """.chigh atp/wta <player> will return the career high ranking of the player."""
+def chigh(text,reply):
+    """<atp/wta> <player> will return the career high ranking of the player."""
     rq = text
     if not rq:
-        return('Format: .chigh atp djokovic')
+        reply('Format: .chigh wta evert')
 
-    # nick_dict = {'shoulders':'Maria%20Sakkari','csn':'Carla%20Suarez%20Navarro','muguruza':'Garbine%20Muguruza'}
+    nick_dict = {'shoulders':'Maria%20Sakkari','muguruza':'Garbine%20Muguruza','azarenka':'Victoria%20Azarenka', \
+        'dave':'Novak%20Djokovic','delpo':'delpo','evert':r'Chris%20Evert','bjk':r'Billie%20Jean%20King'}
 
     rq = rq.strip()
     rq = rq.split(' ')
 
-    if rq[0].lower() == 'atp':
+    if rq[0] == 'atp':
         num = '2'
-    elif rq[0].lower() == 'wta':
+    elif rq[0] == 'wta':
         num = '4'
     else:
-        return('Invalid tour. Please type "atp" or "wta" after the command.')
-	
+        reply('Invalid tour. Please type "atp" or "wta" after the command.')
+
     if len(rq[1:]) > 1:
-        search_text = ' '.join(rq[1:])
+        search_text = '+'.join(rq[1:])
     else:
         search_text = rq[1]
 
-    page = requests.get(f'http://www.tennisexplorer.com/list-players/?search-text-pl={search_text}')
-    tree = html.fromstring(page.text)
-    try:
-        plink_end = tree.xpath(f'//table[@class="result"]/tbody/tr[1]/td[{num}]/a/@href')
-        plink = f'http://www.tennisexplorer.com{plink_end[0]}'
-    except IndexError:
-        return('Could not find player.')
-
-    ppage = requests.get(plink)
-    ptree = html.fromstring(ppage.text)
-
-    try:
-        pname = ptree.xpath('//div[@id="center"]/div/table/tbody/tr/td[2]/h3/text()')[0].split(' ')
-        prank = ptree.xpath('//div[@id="center"]/div/table/tbody/tr/td[2]/div[3]/text()')[0].split(' ')
-        if ')' in prank[-1]:
-            prank = ptree.xpath('//div[@id="center"]/div/table/tbody/tr/td[2]/div[4]/text()')[0].split(' ')
-    except IndexError:
-        print('Valid player page, but could not find rank information.')
-    
-    if 'man' in prank[-1]:
-        chigh_string = f'{pname[-1]} ' + ' '.join(pname[0:-1]) + ' has no ranking information before 1995.'
+    if search_text in nick_dict:
+        name_lookup = nick_dict[search_text]
     else:
-        chigh_string = f'{pname[-1]} ' + ' '.join(pname[0:-1]) + f': Singles career high = #{prank[-1][0:-1]}'
+        page = requests.get(f'http://www.tennisexplorer.com/list-players/?search-text-pl={search_text}')
+        tree = html.fromstring(page.text)
+        try:
+            p1text = tree.xpath(f'//table[@class="result"]/tbody/tr[1]/td[{num}]/a/text()')[0].split(',')
+        except IndexError:
+            reply('Could not find player.')
+        name_lookup = '%20'.join([p1text[1][1:].replace(' ','_'),p1text[0].replace(' ','_')])
 
-    return(chigh_string)
+    if num == '2':
+        url = f'https://www.atptour.com/en/-/ajax/playersearch/PlayerUrlSearch?searchTerm={name_lookup}'
+        player_json = requests.get(url).json()
+        if len(player_json['items']) > 1:
+            reply('Multiple players returned. Please refine your search.')
+            return
+        elif len(player_json['items']) == 0:
+            reply('No players found.')
+            return
+        
+        player_url = f'https://www.atptour.com{player_json["items"][0]["Value"]}'
+        player_name = player_json['items'][0]['Key']
 
+        ppage = requests.get(player_url)
+        ptree = html.fromstring(ppage.text)
+        rank = ptree.xpath('//table[@id="playersStatsTable"]/tbody/tr[2]/td[2]/div[1]')[0].get('data-singles').strip()
+        date_raw = ptree.xpath('//table[@id="playersStatsTable"]/tbody/tr[2]/td[2]/div[2]')[0].get('data-singles-label').strip().split(' ')[-1]
+        date_obj = datetime.datetime.strptime(date_raw,'%Y.%m.%d')
+
+        reply(f'{player_name}: Singles Career High = #{rank} (first reached on {date_obj.strftime("%B")} {date_obj.strftime("%d")}, {date_obj.strftime("%Y")})')
+    else:
+        url = f'https://www.wtatennis.com/search/players/{name_lookup}'
+        search_tree = html.fromstring(requests.get(url).text)
+        player_div = search_tree.xpath('//div[@id="block-luxbox-search-luxbox-search-block-players"]/div[1]/div')
+        if len(player_div) > 1:
+            reply('Multiple players returned. Please refine your search.')
+            return
+        elif len(player_div) == 0:
+            reply('No players found.')
+            return
+        
+        link_div = player_div[0].xpath('div[1]/div[2]/h2/a/@href')[0]
+        link_div = f'{link_div}#ranking'
+        player_name = player_div[0].xpath('div[1]/div[2]/h2/a/text()')[0].strip()
+        ptree = html.fromstring(requests.get(link_div).text)
+
+        rank = ptree.xpath('//div[@class="rankings-container career-high"]/div[1]/p[1]/text()')[0].strip()
+        date_raw = ptree.xpath('//div[@class="rankings-container career-high"]/div[1]/p[2]/text()')[0].strip()
+        date_obj = datetime.datetime.strptime(date_raw,'%Y/%m/%d')
+
+        reply(f'{player_name}: Singles Career High = #{rank} (first reached on {date_obj.strftime("%B")} {date_obj.strftime("%d")}, {date_obj.strftime("%Y")})')
--- plugins/rankhistory.py
+++ plugins/rankhistory.py
@@ -6,58 +6,120 @@ import datetime
 
 @hook.command("rankhistory")
 def rankhistory(text,reply):
-    "<player name> <YYYY-MM-DD> returns the official ATP ranking of the player that week"
+    "<atp/wta> <player name> <YYYY-MM-DD> returns the official singles ranking of the player that week."
 
-    text = text.split(' ')
-    player = '%20'.join(text[0:-1])
-    date = text[-1]
-    atp_url = f'https://www.atptour.com/en/-/ajax/playersearch/PlayerUrlSearch?searchTerm={player}'
+    rq = text.split(' ')
+    if not rq:
+        reply('Format: .rankhistory atp federer')
 
-    player_json = requests.get(atp_url).json()
+    nick_dict = {'shoulders':'Maria%20Sakkari','muguruza':'Garbine%20Muguruza','azarenka':'Victoria%20Azarenka', \
+	'dave':'Novak%20Djokovic','delpo':'delpo','evert':r'Chris%20Evert','bjk':r'Billie%20Jean%20King'}
 
-    if len(player_json['items']) > 1:
-        reply('Multiple players returned. Please refine your search.')
+    if rq[0] == 'atp':
+        num = '2'
+    elif rq[0] == 'wta':
+        num = '4'
+    else:
+        reply('Invalid tour. Please type "atp" or "wta" after the command.')
         return
-    elif len(player_json['items']) == 0:
-        reply('No players found.')
-        return
 
-    split_end = player_json['items'][0]['Value'].split('/')
-    split_end[-1] = 'rankings-history'
-    new_url = '/'.join(split_end)
-    pname = player_json['items'][0]['Key']
+    if len(rq[1:-1]) > 1:
+        search_text = '+'.join(rq[1:-1])
+    else:
+        search_text = rq[1:-1][0]
 
-    hist_url = f'https://www.atptour.com{new_url}'
-    page = requests.get(hist_url)
-    tree = html.fromstring(page.text)
-    rank_rows = tree.xpath('//table[@class="mega-table"]/tbody/tr')
+    date = rq[-1]
     try:
         date_obj = datetime.datetime.strptime(date,'%Y-%m-%d')
     except ValueError:
-        reply('Invalid date format. Must be YYYY-MM-DD.')
+        reply('Invalid date/date format. Must be YYYY-MM-DD.')
         return
-    monday1 = date_obj - datetime.timedelta(days=date_obj.weekday())
-    monday2 = (monday1 - datetime.timedelta(days=1)) - datetime.timedelta(days=(monday1 - datetime.timedelta(days=1)).weekday())
-    wednesday1 = monday2 + datetime.timedelta(days=2)
-    datestr1 = f'{monday1.strftime("%Y")}.{monday1.strftime("%m")}.{monday1.strftime("%d")}'
-    datestr2 = f'{monday2.strftime("%Y")}.{monday2.strftime("%m")}.{monday2.strftime("%d")}'
-    datestr3 = f'{wednesday1.strftime("%Y")}.{wednesday1.strftime("%m")}.{wednesday1.strftime("%d")}'
 
-    for row in rank_rows:
-        site_date = row.xpath('td[1]/text()')[0].strip()
-        if site_date == datestr1:
-            rank = row.xpath('td[2]/text()')[0].strip()
-            reply(f'{pname}: #{rank} on {date_obj.strftime("%B")} {date_obj.strftime("%d")}, {date_obj.strftime("%Y")}')
+    if search_text in nick_dict:
+        name_lookup = nick_dict[search_text]
+    else:
+        page = requests.get(f'http://www.tennisexplorer.com/list-players/?search-text-pl={search_text}')
+        tree = html.fromstring(page.text)
+        try:
+            p1text = tree.xpath(f'//table[@class="result"]/tbody/tr[1]/td[{num}]/a/text()')[0].split(',')
+        except IndexError:
+            reply('Could not find player.')
             return
-        elif site_date == datestr2:
-            rank = row.xpath('td[2]/text()')[0].strip()
-            reply(f'{pname}: #{rank} on {date_obj.strftime("%B")} {date_obj.strftime("%d")}, {date_obj.strftime("%Y")}')
-            return
-        elif site_date == datestr3:
-            rank = row.xpath('td[2]/text()')[0].strip()
-            reply(f'{pname}: #{rank} on {date_obj.strftime("%B")} {date_obj.strftime("%d")}, {date_obj.strftime("%Y")}')
-            return
+        name_lookup = '%20'.join([p1text[1][1:].replace(' ','_'),p1text[0].replace(' ','_')])
 
-    reply('Valid date, but no ranking found.')
-    return
-    
+    if num == '2':
+        url = f'https://www.atptour.com/en/-/ajax/playersearch/PlayerUrlSearch?searchTerm={name_lookup}'
+        player_json = requests.get(url).json()
+        if len(player_json['items']) > 1:
+            reply('Multiple players returned. Please refine your search.')
+            return
+        elif len(player_json['items']) == 0:
+            reply('No players found.')
+            return
+        
+        split_end = player_json['items'][0]['Value'].split('/')
+        split_end[-1] = 'rankings-history'
+        new_url = '/'.join(split_end)
+        pname = player_json['items'][0]['Key']
+
+        hist_url = f'https://www.atptour.com{new_url}'
+        page = requests.get(hist_url)
+        tree = html.fromstring(page.text)
+        rank_rows = tree.xpath('//table[@class="mega-table"]/tbody/tr')
+        monday1 = date_obj - datetime.timedelta(days=date_obj.weekday())
+        monday2 = (monday1 - datetime.timedelta(days=1)) - datetime.timedelta(days=(monday1 - datetime.timedelta(days=1)).weekday())
+        wednesday1 = monday2 + datetime.timedelta(days=2)
+        datestr1 = f'{monday1.strftime("%Y")}.{monday1.strftime("%m")}.{monday1.strftime("%d")}'
+        datestr2 = f'{monday2.strftime("%Y")}.{monday2.strftime("%m")}.{monday2.strftime("%d")}'
+        datestr3 = f'{wednesday1.strftime("%Y")}.{wednesday1.strftime("%m")}.{wednesday1.strftime("%d")}'
+
+        for row in rank_rows:
+            site_date = row.xpath('td[1]/text()')[0].strip()
+            if site_date == datestr1:
+                rank = row.xpath('td[2]/text()')[0].strip()
+                reply(f'{pname}: #{rank} on {date_obj.strftime("%B")} {date_obj.strftime("%d")}, {date_obj.strftime("%Y")}')
+                return
+            elif site_date == datestr2:
+                rank = row.xpath('td[2]/text()')[0].strip()
+                reply(f'{pname}: #{rank} on {date_obj.strftime("%B")} {date_obj.strftime("%d")}, {date_obj.strftime("%Y")}')
+                return
+            elif site_date == datestr3:
+                rank = row.xpath('td[2]/text()')[0].strip()
+                reply(f'{pname}: #{rank} on {date_obj.strftime("%B")} {date_obj.strftime("%d")}, {date_obj.strftime("%Y")}')
+                return
+    else:
+        url = f'https://www.wtatennis.com/search/players/{name_lookup}'
+        search_tree = html.fromstring(requests.get(url).text)
+        player_div = search_tree.xpath('//div[@id="block-luxbox-search-luxbox-search-block-players"]/div[1]/div')
+        if len(player_div) > 1:
+            reply('Multiple players returned. Please refine your search.')
+            return
+        elif len(player_div) == 0:
+            reply('No players found.')
+            return
+        
+        link_div = player_div[0].xpath('div[1]/div[2]/h2/a/@href')[0]
+        pname = player_div[0].xpath('div[1]/div[2]/h2/a/text()')[0].strip()
+        split_link = link_div.split('/')
+        monday1 = date_obj - datetime.timedelta(days=date_obj.weekday())
+        monday2 = (monday1 - datetime.timedelta(days=1)) - datetime.timedelta(days=(monday1 - datetime.timedelta(days=1)).weekday())
+        datestr1 = f'{monday1.strftime("%m")}-{monday1.strftime("%d")}-{monday1.strftime("%Y")}'
+        datestr2 = f'{monday2.strftime("%m")}-{monday2.strftime("%d")}-{monday2.strftime("%Y")}'
+        hist_url = f'https://www.wtatennis.com/player/{split_link[-3]}/print/rankings-history/{monday1.strftime("%Y")}'
+
+        page = requests.get(hist_url)
+        tree = html.fromstring(page.text)
+        rank_rows = tree.xpath('//table[@id="weekly-rankings-table"]/tbody/tr')
+
+        for row in rank_rows:
+            site_date = row.xpath('td[1]/text()')[0].strip()
+            if site_date == datestr1:
+                rank = row.xpath('td[2]/text()')[0].strip()
+                reply(f'{pname}: #{rank} on {date_obj.strftime("%B")} {date_obj.strftime("%d")}, {date_obj.strftime("%Y")}')
+                return
+            elif site_date == datestr2:
+                rank = row.xpath('td[2]/text()')[0].strip()
+                reply(f'{pname}: #{rank} on {date_obj.strftime("%B")} {date_obj.strftime("%d")}, {date_obj.strftime("%Y")}')
+                return
+
+    reply(f'Valid date, but no ranking found for {pname} for that week.')
